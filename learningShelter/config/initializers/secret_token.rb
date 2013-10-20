4c9eb605821299ffa1a165a70cdf65eb4bc2b3d9
saasbook <saasbook@saasbook.(none)>
2013-10-20 15:12:53 -0700

    first commit

---
+++ learningShelter/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LearningShelter::Application.config.secret_token = '70c05e73563b37c7c714b2def090e607d4c3dbf0bc7c4cb13d8c97ffcc776c7fd9130e7151abcd4a19b13bd0a368fd5067037ca4dca85a347e46472512526a54'
